4b583bf674f2e69181a9ce85ebe37182c0e91d29
developers@quapton.com <developers@quapton.com>
2025-06-26 18:56:11 +0530

    updated css and desktop

--- travel_management/config/desktop.py
+++ travel_management/config/desktop.py
@@ -6,5 +6,7 @@ def get_data():
             "module_name": "Travel Management",
             "type": "module",
             "label": _("Travel Management"),
+            "color": "#3498db",            # Choose your color
+            "icon": "octicon octicon-globe",  # Or choose another Octicon
         }
     ]
