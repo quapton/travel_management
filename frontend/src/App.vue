bdf51a069b8e40fab45f6bde6a501deb1c1fe26f
developers@quapton.com <developers@quapton.com>
2025-06-26 20:19:46 +0530

    udated app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <FrappeUIProvider>
-    <Layout>
-      <div class="text-base">
-        <router-view />
-      </div>
-    </Layout>
+    <div class="p-4">
+      <h1>Welcome to Travel Management App</h1>
+      <p>This is a simple Vue + Frappe UI application.</p>
+    </div>
     <Dialogs />
   </FrappeUIProvider>
 </template>
@@ -12,35 +11,4 @@
 <script setup>
 import { FrappeUIProvider } from 'frappe-ui'
 import { Dialogs } from '@/utils/dialogs'
-import { computed, onUnmounted, ref, watch } from 'vue'
-import { useScreenSize } from './utils/composables'
-import DesktopLayout from '@/components/DesktopLayout.vue'
-import MobileLayout from '@/components/MobileLayout.vue'
-import NoSidebarLayout from '@/components/NoSidebarLayout.vue'
-import { usersStore } from '@/stores/user'
-import { useRouter } from 'vue-router'
-import { posthogSettings } from '@/telemetry'
-
-const screenSize = { width: 1024 } 
-const router = useRouter()
-const noSidebar = ref(false)
-const { userResource } = usersStore()
-
-router.beforeEach((to, from, next) => {
-  noSidebar.value = to.query.fromLesson || to.path === '/persona'
-  next()
-})
-
-const Layout = computed(() => {
-  if (noSidebar.value) return NoSidebarLayout
-  return screenSize.width < 640 ? MobileLayout : DesktopLayout
-})
-
-onUnmounted(() => {
-  noSidebar.value = false
-})
-
-watch(userResource, () => {
-  if (userResource.data) posthogSettings.reload()
-})
 </script>
