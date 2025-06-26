ff499cbd51b1844380063d72025ff52223ce8128
developers@quapton.com <developers@quapton.com>
2025-06-26 16:27:39 +0530

    updated frontend module

---
+++ frontend/vite.config.js
@@ -0,0 +1,62 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
+import frappeui from 'frappe-ui/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    frappeui({
+      frappeProxy: true,
+      lucideIcons: true,
+      jinjaBootData: true,
+      buildConfig: {
+        indexHtmlPath: '../travel_management/www/travel_management.html',
+        emptyOutDir: true,
+        sourcemap: true,
+      },
+    }),
+    vue(),
+    vueJsx(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        display: 'standalone',
+        name: 'Travel Management',
+        short_name: 'Travel Management',
+        start_url: '/travel_management',
+        description:
+          'Modern & 100% Open-source Travel Management tool to supercharge your sales operations',
+        icons: [
+          {
+            src: '/assets/travel_management/images/travel.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+         
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'feather-icons',
+      'showdown',
+      'tailwind.config.js',
+      'prosemirror-state',
+      'prosemirror-view',
+      'lowlight',
+    ],
+  },
+})
